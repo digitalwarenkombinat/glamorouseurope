d970257ff40ac5e8a98fb24d4928746999de3137
Gerd Müller <gerdesque@posteo.org>
2023-09-07 09:54:43 +0200

    Add pwa requirements

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,26 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "GLAMorous Europe",
+        short_name: "GLAM EU",
+        description: "Wir machen Kulturerbe digital erfahrbar.",
+        theme_color: "#f09",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
     }),
   ],
 });
