3a60dcd9ed3847dad3a403578c8d060ea329867a
Gerd Müller <gerdesque@posteo.org>
2023-09-22 14:12:41 +0200

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/glamorouseurope/",
+  build: {
+    assetsDir: "",
+  },
   plugins: [
     react(),
     VitePWA({
