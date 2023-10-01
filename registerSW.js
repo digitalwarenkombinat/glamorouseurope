2595bc5f4ffa8a621abf99a39498e184159b186e
gerdesque <gerdesque@users.noreply.github.com>
2023-10-01 14:32:28 +0000

    Deploying to gh-pages from @ digitalwarenkombinat/glamorouseurope@26c4e988ba9635dd7795ac0b24e70c148bb3817c 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/glamorouseurope/sw.js', { scope: '/glamorouseurope/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
