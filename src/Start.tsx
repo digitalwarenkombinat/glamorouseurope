3a44369c848be7fd2c517927721bfc74a39f3911
Gerd Müller <gerdesque@posteo.org>
2023-11-18 14:35:20 +0100

    Adjust start component

--- src/Start.tsx
+++ src/Start.tsx
@@ -1,16 +1,16 @@
 // @ts-ignore
-import { Block, Button, Link } from "konsta/react";
-import { NavLink } from "react-router-dom";
+import { Block } from "konsta/react";
+// import LogoIcon from "/logo_neon.svg";
+
+const Logo = () => {
+  return <img src={LogoIcon} alt="Logo GLAMorous Europe" />;
+};
 
 function Start() {
   return (
     <Block className="flex flex-col gap-16 container mx-auto justify-center content-evenly">
-      <h1 className="neon neonFont">GLAMorouS Europe</h1>
-      <Link to="/collect" component={NavLink}>
-        <Button large rounded className="k-color-brand-secondary">
-          Collect the items
-        </Button>
-      </Link>
+      <h1 className="neon neonFont">GLAM</h1>
+      {/* <Logo /> */}
     </Block>
   );
 }
