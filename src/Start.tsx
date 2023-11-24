ce461013d2e0ca27afd3a4ffbd01bb24b84b4eff
Gerd Müller <gerdesque@posteo.org>
2023-11-24 18:53:47 +0100

    Add collage image

--- src/Start.tsx
+++ src/Start.tsx
@@ -1,15 +1,21 @@
 // @ts-ignore
 import { Block } from "konsta/react";
 import LogoIcon from "/logo_animated.svg";
+import CollageIcon from "/collage.webp";
 
 const Logo = () => {
   return <img src={LogoIcon} alt="Logo GLAMorous Europe" />;
 };
 
+const Collage = () => {
+  return <img src={CollageIcon} alt="Collage GLAMorous Europe" />;
+};
+
 function Start() {
   return (
-    <Block className="flex flex-col gap-16 container mx-auto justify-center content-evenly">
+    <Block className="flex flex-col my-0 gap-4 container mx-auto justify-center content-evenly">
       <Logo />
+      <Collage />
     </Block>
   );
 }
