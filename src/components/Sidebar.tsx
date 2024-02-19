671c00ddf01db1885b3e94a22233b89f74054cb2
Gerd Müller <gerdesque@posteo.org>
2024-02-19 11:31:31 +0100

    🐛 fix(app): adjust sidebar translations

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,6 @@
+import { XMarkIcon } from "@heroicons/react/24/outline";
 // @ts-expect-error konsta typing
-import { Block, Link, Navbar, Page, Panel } from "konsta/react";
+import { Block, Navbar, Page, Panel } from "konsta/react";
 import { SetStateAction } from "react";
 import { useTranslation } from "react-i18next";
 
@@ -16,16 +17,16 @@ export default function Sidebar(
     >
       <Page>
         <Navbar
-          title={t("start")}
           right={
-            <Link navbar onClick={() => setPanelOpened(false)}>
-              {t("start")}
-            </Link>
+            <XMarkIcon
+              onClick={() => setPanelOpened(false)}
+              className="mr-4 w-10 h-10"
+            />
           }
         />
         <Block className="space-y-4">
-          <p>{t("start")}</p>
-          <p>{t("start")}</p>
+          <p>{t("sidebarAbout")}</p>
+          <p>{t("sidebarLegal")}</p>
         </Block>
       </Page>
     </Panel>
