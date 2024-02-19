7659a4fef1cfd4a7c249af5febf6f6da4a26078a
Gerd Müller <gerdesque@posteo.org>
2024-02-19 12:15:13 +0100

    ✨ feat(app): add support logos in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,56 @@
 import { XMarkIcon } from "@heroicons/react/24/outline";
 // @ts-expect-error konsta typing
-import { Block, Navbar, Page, Panel } from "konsta/react";
+import { Block, Button, Link, Navbar, Page, Panel } from "konsta/react";
 import { SetStateAction } from "react";
 import { useTranslation } from "react-i18next";
+
+import LogoBMBFIconDE from "/logos/bmbf_logo_de.jpg";
+import LogoBMBFIconEN from "/logos/bmbf_logo_en.jpg";
+import LogoDigitalwarenkombinatIcon from "/logos/digitalwarenkombinat.png";
+import LogoPrototypeFundIcon from "/logos/prototype_fund_logo.png";
+
+const LogoPrototypeFund = () => {
+  return (
+    <Link href={"https://prototypefund.de/"} target="_blank">
+      <img
+        src={LogoPrototypeFundIcon}
+        alt="Logo Protype Fund"
+        className="w-96 m-auto"
+      />
+    </Link>
+  );
+};
+
+const LogoBMBF = () => {
+  const { i18n } = useTranslation();
+  return (
+    <Link href={"https://www.bmbf.de/"} target="_blank">
+      <img
+        src={i18n.resolvedLanguage === "en" ? LogoBMBFIconEN : LogoBMBFIconDE}
+        alt="Logo Bundesministerium für Bildung und Forschung"
+        className="w-96 m-auto"
+      />
+    </Link>
+  );
+};
+
+const LogoDigitalwarenkombinat = () => {
+  return (
+    <Link href={"https://digitalwarenkombinat.de/"} target="_blank">
+      <img
+        src={LogoDigitalwarenkombinatIcon}
+        alt="Logo Digitalwarenkombinat"
+        className="w-96 m-auto"
+      />
+    </Link>
+  );
+};
 
 export default function Sidebar(
   panelOpened: boolean,
   setPanelOpened: (value: SetStateAction<boolean>) => void,
 ) {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   return (
     <Panel
       side="right"
@@ -25,8 +67,28 @@ export default function Sidebar(
           }
         />
         <Block className="space-y-4">
+          <p className="text-xl">{t("sidebarChooseLanguage")}</p>
+          <Button
+            onClick={() => i18n.changeLanguage("de")}
+            className="p-4 text-xl text-black w-10 mr-2 h-10"
+            rounded
+            inline
+          >
+            DE
+          </Button>
+          <Button
+            onClick={() => i18n.changeLanguage("en")}
+            className="p-4 text-xl text-black w-10 h-10"
+            rounded
+            inline
+          >
+            EN
+          </Button>
           <p>{t("sidebarAbout")}</p>
           <p>{t("sidebarLegal")}</p>
+          <LogoPrototypeFund />
+          <LogoBMBF />
+          <LogoDigitalwarenkombinat />
         </Block>
       </Page>
     </Panel>
