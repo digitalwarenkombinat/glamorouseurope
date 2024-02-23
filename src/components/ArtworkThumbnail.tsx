a1ad552b2b9ffc8c5f52f3724c3092e7838a9387
Gerd Müller <gerdesque@posteo.org>
2024-02-23 18:59:25 +0100

    ✨ feat(app): use multi backend drag and drag

--- src/components/ArtworkThumbnail.tsx
+++ src/components/ArtworkThumbnail.tsx
@@ -1,16 +1,42 @@
+import { CSSProperties } from "react";
 import { DragPreviewImage, useDrag } from "react-dnd";
+import { Preview } from "react-dnd-multi-backend";
 
 function ArtworkThumbnail({ id, image }: { id: string; image: string }) {
   const [{ opacity }, drag, preview] = useDrag(() => ({
     type: "artwork",
-    item: { id },
+    item: { id, image },
     collect: (monitor) => ({
       opacity: monitor.isDragging() ? 0.4 : 1,
     }),
   }));
+
+  const generatePreview = ({
+    item,
+    style,
+  }: {
+    item: { id: string; image: string };
+    style: CSSProperties;
+  }) => {
+    return (
+      <img
+        src={item.image}
+        alt={item.id}
+        style={{
+          ...style,
+          cursor: "move",
+          maxHeight: "100px",
+          width: "auto",
+          zIndex: 99,
+        }}
+      />
+    );
+  };
+
   return (
     <>
       <DragPreviewImage connect={preview} src={image} />
+      <Preview>{generatePreview}</Preview>
       <img
         src={image}
         alt={id}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import i18next from "i18next";
 import React from "react";
-import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import {
+  DndProvider,
+  MouseTransition,
+  TouchTransition,
+} from "react-dnd-multi-backend";
 import { TouchBackend } from "react-dnd-touch-backend";
 import { createRoot } from "react-dom/client";
 import { I18nextProvider } from "react-i18next";
@@ -20,15 +25,35 @@ i18next.init({
   },
 });
 
-const options = {
-  scrollAngleRanges: [{ start: 300 }, { end: 60 }, { start: 120, end: 240 }],
-  ignoreContextMenu: true,
+const HTML5toTouch = {
+  backends: [
+    {
+      id: "html5",
+      backend: HTML5Backend,
+      transition: MouseTransition,
+    },
+    {
+      id: "touch",
+      backend: TouchBackend,
+      options: {
+        delay: 50,
+        ignoreContextMenu: true,
+        scrollAngleRanges: [
+          { start: 300 },
+          { end: 60 },
+          { start: 120, end: 240 },
+        ],
+      },
+      preview: true,
+      transition: TouchTransition,
+    },
+  ],
 };
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18next}>
-      <DndProvider backend={TouchBackend} options={options}>
+      <DndProvider options={HTML5toTouch}>
         <App />
       </DndProvider>
     </I18nextProvider>
