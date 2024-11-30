c6a0bb16c11bfd158d0dfa6491da39441d883da3
Gerd Müller <gerdesque@posteo.org>
2024-11-30 13:49:52 +0100

    ✨ feat(app): adjust query for european data as weekly cronjob

--- server/fetchData.cjs
+++ server/fetchData.cjs
@@ -1,29 +1,31 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
-/* eslint-disable @typescript-eslint/no-require-imports */
 const axios = require("axios");
 const fs = require("fs");
-const { fetchIIIFIdentifier } = require("./fetchIIIFIdentifier");
+const { fetchIIIFIdentifier } = require("./fetchIIIFIdentifier.cjs");
 
 const url = "https://query.wikidata.org/sparql";
+// ${ favouriteMaterial !== "" ? `FILTER ( ?material = ${favouriteMaterial} )` : "" }
+// ?collection wdt:P17 ?country.
 const query = `
- SELECT ?item ?itemLabel ?countryLabel ?collectionLabel ?year ?locationLabel ?creatorLabel ?iiifManifest
+  SELECT ?item ?itemLabel ?countryLabel ?genreLabel ?year ?locationLabel ?creatorLabel ?materialLabel ?depictsLabel ?collectionLabel ?iiifManifest
   WITH {
-    SELECT ?item (SAMPLE(?country) as ?country) (SAMPLE(YEAR(?date)) as ?year) (SAMPLE(?location) as ?location) (SAMPLE(?creator) as ?creator) (SAMPLE(?collection) as ?collection) (SAMPLE(?iiifManifest) as ?iiifManifest)
+    SELECT ?item (SAMPLE(?country) as ?country) (SAMPLE(?genre) as ?genre) (SAMPLE(YEAR(?inception)) as ?year) (SAMPLE(?location) as ?location) (SAMPLE(?creator) as ?creator) (SAMPLE(?material) as ?material) (SAMPLE(?depicts) as ?depicts) (SAMPLE(?collection) as ?collection) (SAMPLE(?iiifManifest) as ?iiifManifest)
     WHERE {
       hint:Query hint:optimizer "None".
       ?item wdt:P6108 ?iiifManifest;
-            wdt:P495 ?originCountry;
-            wdt:P195 ?collection;
+            wdt:P495 ?country.
+      wd:Q458 wdt:P150 ?country.
+      ?item wdt:P195 ?collection;
             (wdt:P31/(wdt:P279*)) wd:Q110304307.
             hint:Prior hint:gearing "forward".
 
-      ?collection wdt:P17 ?collectionCountry.
-      BIND(COALESCE(?originCountry, ?collectionCountry) AS ?country)
-      ?country wdt:P47 wd:Q183.
-      
-      OPTIONAL { ?item wdt:P571 ?date. }
+      OPTIONAL { ?item wdt:P136 ?genre. }
+      OPTIONAL { ?item wdt:P571 ?inception. }
       OPTIONAL { ?item wdt:P276 ?location. }
       OPTIONAL { ?item wdt:P170 ?creator. }
+      OPTIONAL { ?item wdt:P186 ?material. }
+      OPTIONAL { ?item wdt:P180 ?depicts. }
     }
     GROUP BY ?item
     LIMIT 1000
@@ -70,6 +72,10 @@ const formatImageData = (element, imageURL) => ({
   name: element.itemLabel?.value || "",
   year: element.year?.value || "",
   country: element.countryLabel?.value || "",
+  genre: element.genreLabel?.value || "",
+  material: element.materialLabel?.value || "",
+  depicts: element.depictsLabel?.value || "",
+  collection: element.collectionLabel?.value || "",
   location: element.locationLabel?.value || "",
   creator: element.creatorLabel?.value || "",
   url: element.iiifManifest.value,
